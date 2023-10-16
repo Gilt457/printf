689177ee3ed3c553949b0db2bb8b449b890f1041
Jeraymi <99403069+Jeraymi@users.noreply.github.com>
2023-10-17 00:25:06 +0100

    Jeraymi made some changes

--- _printf.c
+++ _printf.c
@@ -1,22 +1,11 @@
 #include "main.h"
 
-/**
- * print_buffer - Writes the contents of the buffer to standard output if it's not empty.
- * @buffer: Array of characters.
- * @buff_ind: Index representing the length of the buffer.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
-}
+void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Custom printf function that handles format strings.
- * @format: The format string.
- * Return: The number of characters printed.
+ * _printf - Printf function
+ * @format: format
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
@@ -25,18 +14,19 @@ int _printf(const char *format, ...)
 	va_list list;
 	char buffer[BUFF_SIZE];
 
-	if (!format)
+	if (format == NULL)
 		return (-1);
 
 	va_start(list, format);
 
-	for (i = 0; format[i]; i++)
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
@@ -46,7 +36,7 @@ int _printf(const char *format, ...)
 			width = get_width(format, &i, list);
 			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
-			i++;
+			++i;
 			printed = handle_print(format, &i, list, buffer,
 				flags, width, precision, size);
 			if (printed == -1)
@@ -60,4 +50,17 @@ int _printf(const char *format, ...)
 	va_end(list);
 
 	return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
